9da94c59a14c903d3f393f8cb3f4ec487db87313
sonbeo1999hd <43996384+sonbeo1999hd@users.noreply.github.com>
2019-09-12 11:44:49 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var m = require("mithril")
+
+var UserList = require("./src/views/UserList");
+var UserForm = require("./src/views/UserForms");
+var Layout = require("./src/views/Layout");
+
+m.route(document.body, "/list", {
+    "/list": {
+        render: function() {
+            return m(Layout, m(UserList))
+        }
+    },
+    "/edit/:id": {
+        render: function(vnode) {
+            return m(Layout, m(UserForm, vnode.attrs))
+        }
+    },
+})
